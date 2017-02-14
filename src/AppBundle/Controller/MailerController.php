77fe9f4efbc55a67f3aa4b91db57406c59d319b9
pavlovicnemanja <me@pavlovicnemanja.com>
2017-02-14 16:02:55 +0100

    feat(booking): Added sending booking and confirmation mails

--- src/AppBundle/Controller/MailerController.php
+++ src/AppBundle/Controller/MailerController.php
@@ -20,7 +20,40 @@ class MailerController extends FOSRestController {
 
         $this->get('mailer')->send($message);
 
-        return $this->handleView($this->view($data));
+        $view = array(
+            'status' => 200,
+            'message' => 'Message sent'
+        );
+
+        return $this->handleView($this->view($view));
+    }
+
+    public function bookingAction(Request $request) {
+        $data = $request->request->all();
+        $reservation_id = uniqid();
+
+        $message = \Swift_Message::newInstance()
+                ->setSubject('Website - New Booking!')
+                ->setFrom($data['from'])
+                ->setTo($data['to'])
+                ->setBody($this->renderView('Emails/booking.html.twig', array_merge($data['body'], array('reservation_id' => $reservation_id))), 'text/html');
+
+        $this->get('mailer')->send($message);
+
+        $confirmation = \Swift_Message::newInstance()
+                ->setSubject('Request for booking received!')
+                ->setFrom($data['from'])
+                ->setTo($data['body']['email'])
+                ->setBody($this->renderView('Emails/booking.confirmation.html.twig', array_merge($data['body'], array('reservation_id' => $reservation_id))), 'text/html');
+
+        $this->get('mailer')->send($confirmation);
+
+        $view = array(
+            'status' => 200,
+            'message' => 'Booking confirmation send'
+        );
+
+        return $this->handleView($this->view($view));
     }
 
 }
